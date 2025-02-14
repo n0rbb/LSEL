aa43390cd2d205b72f697d9c94c4e56a3eda30a5
Daniel Muñiz <d.muniz@alumnos.upm.es>
2025-02-14 16:42:24 +0100

    Time drift correction and comment addition

--- P1/P1-2/MQTT-datetimepub.py
+++ P1/P1-2/MQTT-datetimepub.py
@@ -1,6 +1,5 @@
 import paho.mqtt.client as mqtt # Import the MQTT library
 import time # The time library is useful for delays
-from itertools import cycle
 from datetime import datetime
 import sys
 
@@ -55,12 +54,19 @@ else:
 client_id = f"{client_id} > {mode}"
 
 ourClient = mqtt.Client(client_id) # Create a MQTT client object
-ourClient.connect("10.8.42.100", 1885) # Connect to the test MQTT broker
+ourClient.connect("10.8.42.19", 1885) # Connect to the test MQTT broker
 ourClient.loop_start() # Start the MQTT client
 
+#initialize stored_seconds with an unattainable value
+stored_seconds = 80
 # Main program loop
 while(1):
     now = datetime.now()
-    if now.second % 10 == 0:
+    #Showing time or date each 10 second multiplicand
+    if now.second % 10 == 0 and now.second != stored_seconds:
         publish_msg(ourClient, mode) # Publish message to MQTT broker
-    time.sleep(1) # Sleep for 10 seconds
+        #Storing seconds to avoid a problem where 10 multiplicands would be skipped with a sleep of 1 second or more
+        #This couldn't be resolved even using techniques like taking time before and after the execution to
+        #compensate for execution time drifts, the only solution was to use a sleep time of less than 1 second
+        stored_seconds = now.second
+    time.sleep(0.5) # Sleep for 0.2 seconds (to guarantee that there is no skip in a 10 multiplicand)
