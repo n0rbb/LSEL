ef3012b931f242d006ed67b4a49700e1931e5d06
Mario de Miguel <57144439+n0rbb@users.noreply.github.com>
2025-02-14 12:16:41 +0100

    Add both mode for publisher of datetime
    Definition of function publish_msg which obtains date, time and calls mqtt publish function
    
    Takes as arguments the client: ourClient (MQTT Client) and the mode (d, t, b), this last argument defining both the topic where it is to publish and the content

--- P1/P1-2/MQTT-datetimepub.py
+++ P1/P1-2/MQTT-datetimepub.py
@@ -5,10 +5,6 @@ from datetime import datetime
 import sys
 
 mode=sys.argv[1]
-if mode == "t" or mode == "h":
-	client_topic = 'MQTT_NETWORK/DMZ/TIME'
-if mode == "d":
-	client_topic = 'MQTT_NETWORK/DMZ/DATE'
 
 def get_date():
     now = datetime.now()
@@ -32,21 +28,24 @@ ourClient = mqtt.Client("ZMD") # Create a MQTT client object
 ourClient.connect("10.8.42.100", 1885) # Connect to the test MQTT broker
 ourClient.loop_start() # Start the MQTT client
 
+def publish_msg(ourClient, mode):
+	topics = ['MQTT_NETWORK/DMZ/TIME', 'MQTT_NETWORK/DMZ/DATE']
+	now = datetime.now()
+	day = get_date()
+	if mode == "t" or mode == "h":
+		tim = f"It’s {now.strftime('%H:%M:%S %p')}"
+		ourClient.publish(time_topic, tim)
+		
+	elif mode == "d":
+		ourClient.publish(day_topic, day)
+	
+	elif mode == "b": #Case to publish both	
+		ourClient.publish(topics[0], tim)
+		ourClient.publish(topics[1], day)
+	
+		
 # Main program loop
 while(1):
-	now = datetime.now()
-	if mode == "t" or mode == "h":
-		current_datetime = f"It’s {now.strftime('%H:%M:%S %p')}"
-	if mode == "d":
-		if now.day == 1:
-			suffix = "st"
-		elif now.day == 2:
-			suffix = "nd"
-		elif now.day == 3:
-			suffix = "rd"
-		else:
-			suffix = "th"
-		current_datetime = f"It’s {now.strftime('%A')}, the {now.day}{suffix} of {now.strftime('%B')} of {now.year}"
 	if now.second % 10 == 0:
-		ourClient.publish(client_topic, current_datetime) # Publish message to MQTT broker
+		publish_msg(ourClient, mode) # Publish message to MQTT broker
 	time.sleep(1) # Sleep for 10 seconds
