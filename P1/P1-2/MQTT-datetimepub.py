6df98c988bc7d547801cb73d989d1a1e7faccec5
Daniel Muñiz <d.muniz@alumnos.upm.es>
2025-02-14 13:25:12 +0100

    Revert "Para revertir"
    This reverts commit fbff37e09b24843b1ecf81b8beaf3cd7ffc6099c.

--- P1/P1-2/MQTT-datetimepub.py
+++ P1/P1-2/MQTT-datetimepub.py
@@ -4,10 +4,12 @@ from itertools import cycle
 from datetime import datetime
 import sys
 
-client_topic_hora = 'MQTT_NETWORK/DMZ/TIME'
-client_topic_fecha = 'MQTT_NETWORK/DMZ/TIME'
-
 mode=sys.argv[1]
+if mode == "t" or mode == "h":
+	client_topic = 'MQTT_NETWORK/DMZ/TIME'
+if mode == "d":
+	client_topic = 'MQTT_NETWORK/DMZ/DATE'
+
 def get_date():
     now = datetime.now()
     if now.day == 1:
@@ -27,17 +29,14 @@ def messageFunction (client, userdata, message):
 	print(message)
  
 ourClient = mqtt.Client("ZMD") # Create a MQTT client object
-ourClient.connect("10.8.42.19", 1885) # Connect to the test MQTT broker
+ourClient.connect("10.8.42.100", 1885) # Connect to the test MQTT broker
 ourClient.loop_start() # Start the MQTT client
 
 # Main program loop
 while(1):
 	now = datetime.now()
-	#if now.second % 10 == 0:
 	if mode == "t" or mode == "h":
 		current_datetime = f"It’s {now.strftime('%H:%M:%S %p')}"
-		ourClient.publish(client_topic_hora, current_datetime) # Publish message to MQTT broker
-		time.sleep(2) # Sleep for 2 seconds
 	if mode == "d":
 		if now.day == 1:
 			suffix = "st"
@@ -48,5 +47,6 @@ while(1):
 		else:
 			suffix = "th"
 		current_datetime = f"It’s {now.strftime('%A')}, the {now.day}{suffix} of {now.strftime('%B')} of {now.year}"
-		ourClient.publish(client_topic_fecha, current_datetime) # Publish message to MQTT broker
-		time.sleep(2) # Sleep for 2 seconds
+	if now.second % 10 == 0:
+		ourClient.publish(client_topic, current_datetime) # Publish message to MQTT broker
+	time.sleep(1) # Sleep for 10 seconds
--- P1/P1-2/MQTT-datetimesub.py
+++ P1/P1-2/MQTT-datetimesub.py
@@ -10,7 +10,7 @@ def messageFunction (client, userdata, message):
 	print(message)
  
 ourClient = mqtt.Client("DMZ") # Create a MQTT client object
-ourClient.connect("10.8.42.19", 1885) # Connect to the test MQTT broker
+ourClient.connect("10.8.42.100", 1885) # Connect to the test MQTT broker
 ourClient.subscribe("MQTT_NETWORK/DMZ/#") # Subscribe to the topic AC_unit
 ourClient.on_message = messageFunction # Attach the messageFunction to subscription
 ourClient.loop_start() # Start the MQTT client
